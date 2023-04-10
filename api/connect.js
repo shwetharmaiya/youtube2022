84cbd6d9e844d40fef5796abc2d1dfdfc7779db2
Shwetha <shwetharmaiya@gmail.com>
2023-04-10 11:28:47 +0530

    Update connect.js

--- api/connect.js
+++ api/connect.js
@@ -1,8 +1,8 @@
 import mysql from "mysql"
 
 export const db = mysql.createConnection({
-  host:"localhost",
-  user:"root",
-  password:"root",
+  host:"aws.connect.psdb.cloud",
+  user:"1dci82tmaahhfhxp5imm",
+  password:"pscale_pw_wDVEfXO6C44aXzjb2FWSV6ual6vuuAFnMNgIBmvLdAD",
   database:"social"
 })
